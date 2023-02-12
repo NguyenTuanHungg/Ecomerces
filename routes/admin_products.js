cc6a3be3fd93b54180049dfba13d6f28b0a4c748
ThilinhHao <114476582+NguyenTuanHungg@users.noreply.github.com>
2023-02-12 22:22:01 +0700

    update admin_product.js

--- routes/admin_products.js
+++ routes/admin_products.js
@@ -8,6 +8,32 @@ const Product=mongoose.model('products',products)
 const orders=require('../models/orders')
 let Order=mongoose.model('orders',orders)
 
+
+router.get('/admin_products', function(req, res, next) {
+  const resultsPerPage=10
+   Product.find({},(error,data)=>{
+     console.log('ds:',data)
+     const numOfResults=result.length
+     const numberOfPages= Math.ceil(numOfResults/resultsPerPage)
+     let page=req.query.page ? Number(req.query.page):1;
+     if(page>numberOfPages){
+       res.redirect('/admin_products?page='+encodeURIComponent(numberOfPages))
+     }
+     else if(page<1){
+       res.redirect('/admin_products?page='+encodeURIComponent('1'))
+     }
+   
+   const startingLimit=(page - 1)*resultsPerPage
+   Product.find({},(error,data) => {
+    let iterator=(page-5)<1 ? 1:page-5
+    let endingLink=(iterator+9) <= numberOfPages ? (iterator+9):page+(numberOfPages-page)
+    if(endingLink<(page+4)){
+     iterator-=(page+4)-numberOfPages
+    }
+   }).limit(startingLimit,resultsPerPage) 
+   res.render('admin_products',{datas:data,page,iterator,endingLink,numberOfPages})
+ });
+ })
 //add
 router.get('/form-add',function(req,res){
     res.render('form-add')
